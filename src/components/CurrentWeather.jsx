0f8110c92cda4736c9e61c8aa50066b4460afb0c
Octosari <octosari@outlook.com>
2022-11-28 12:02:36 +0100

    add new icon and improve current weather section

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -2,41 +2,56 @@ import clouds from "../icons/cloud.svg";
 import rain from "../icons/rain.svg";
 import clearDay from "../icons/clearDay.svg";
 import clearNight from "../icons/clearNight.svg";
+import mist from "../icons/mist.svg";
 
 const CurrentWeather = ({ data }) => {
   const currentDate = new Date();
   const image = (data) => {
-    if (data != null && data.weather[0].main == "Clouds") return clouds;
-    else if (data != null && data.weather[0].main == "Rain") return rain;
+    if (data != null && data.weather[0].main === "Clouds") return clouds;
+    else if (data != null && data.weather[0].main === "Rain") return rain;
     else if (
       data != null &&
-      data.weather[0].main == "Clear" &&
-      currentDate.getHours() > 20
+      data.weather[0].main === "Clear" &&
+      currentDate.getHours() > 20 &&
+      currentDate.getHours() < 4
     )
       return clearNight;
     else if (
       data != null &&
-      data.weather[0].main == "Clear" &&
-      currentDate.getHours() < 20
+      data.weather[0].main === "Clear" &&
+      currentDate.getHours() < 20 &&
+      currentDate.getHours() > 4
     )
       return clearDay;
+    else if (
+      (data != null && data.weather[0].main === "Mist") ||
+      (data != null && data.weather[0].main === "Fog")
+    )
+      return mist;
   };
+
   return (
     <div className="currentWeather">
-      <div className="textContainer">
-        <span>{data != null ? data.main.temp : null} ºC</span>
-        <span>{data != null ? data.weather[0].main : null}</span>
-        <span className="currentDate">
-          {currentDate.getUTCFullYear()}.{currentDate.getUTCMonth() + 1}.
-          {currentDate.getUTCDate()}
-        </span>
-      </div>
+      {data != null ? (
+        <>
+          <div className="textContainer">
+            <span>{data != null ? data.main.temp : null} ºC</span>
+            <span>{data != null ? data.weather[0].main : null}</span>
+            <span className="currentDate">
+              {currentDate.getUTCFullYear()}.{currentDate.getUTCMonth() + 1}.
+              {currentDate.getUTCDate()}
+            </span>
+          </div>
 
-      <img
-        src={data != null ? image(data) : null}
-        alt="cloudy"
-        draggable="false"
-      />
+          <img
+            src={data != null ? image(data) : null}
+            alt="cloudy"
+            draggable="false"
+          />
+        </>
+      ) : (
+        <div className="wrongCity">City no found</div>
+      )}
     </div>
   );
 };
